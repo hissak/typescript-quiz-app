d146ac2161c2e2f185a82e1489fd23a2b1aab31f
hissak <hassan.a.issak@gmail.com>
2023-10-08 22:03:42 -0400

    Add fonts to index.html. Create src folder with components. Add content to App component

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+const startTrivia = async () => {
+}
+
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+}
+
+const nextQuestion = () => {
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img/>
-        <p>
-          Test
-        </p>
-        <a>
-          Quiz App with TS and ReactJS
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Question ...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
